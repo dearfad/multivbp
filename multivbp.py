bfb2106ba60b3a91e5ea41b9b7d24875f8acc3fc
dearfad <dearfad@gmail.com>
2024-02-01 21:51:41 +0800

    update

--- multivbp.py
+++ multivbp.py
@@ -3,3 +3,12 @@ import streamlit as st
 st.info('Multi LLMs Chat for Virtual Breast Patient')
 
 inquiry = st.chat_input()
+
+llms = st.columns(3)
+
+with llms[0]:
+    st.write('0')
+
+
+with llms[1]:
+    st.write('1')
