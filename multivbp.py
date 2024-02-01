5f79b28a2fbfa98b52935e625f77b71d2375cf78
dearfad <dearfad@gmail.com>
2024-02-01 21:57:40 +0800

    update

--- multivbp.py
+++ multivbp.py
@@ -6,7 +6,7 @@ inquiry = st.chat_input()
 
 LLMS = ['ZhipuAI', 'qwen']
 
-chat_windows = st.columns(lens(LLMS))
+chat_windows = st.columns(len(LLMS))
 
 for index, chat_window in enumerate(chat_windows):
     with chat_window:
