cbdaf24bbce44962b4f1aaa1aca4659f8d19b025
dearfad <dearfad@gmail.com>
2024-02-02 10:22:45 +0800

    update

--- multivbp.py
+++ multivbp.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from zhipuai import ZhipuAI
 
 st.set_page_config(layout="wide")
 
@@ -31,7 +32,28 @@ for index, chat_col in enumerate(chat_cols):
                 with st.chat_message(message["role"]):
                     st.text(message["content"])
 
+
+def zhipuai(messages):
+    client = ZhipuAI(api_key=st.secrets['zhipuai'])
+    response = client.chat.completions.create(
+        model="glm-4",
+        messages=messages,
+    )
+    return response
+
 if inquiry:
     for llm in LLMS:
         st.session_state[llm].append({'role': 'user', 'content': inquiry})
+        if llm=='ZhipuAI':
+            response = zhipuai(st.session_state[llm])
+            st.write(response)
     st.rerun()
+
+
+    [
+            {"role": "user", "content": "作为一名营销专家，请为我的产品创作一个吸引人的slogan"},
+            {"role": "assistant", "content": "当然，为了创作一个吸引人的slogan，请告诉我一些关于您产品的信息"},
+            {"role": "user", "content": "智谱AI开放平台"},
+            {"role": "assistant", "content": "智启未来，谱绘无限一智谱AI，让创新触手可及!"},
+            {"role": "user", "content": "创造一个更精准、吸引人的slogan"}
+        ]
