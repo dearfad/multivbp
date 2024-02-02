6c9639ab6e558f2b06b75f38e3990a5f276f7a44
dearfad <dearfad@gmail.com>
2024-02-02 10:30:50 +0800

    update

--- multivbp.py
+++ multivbp.py
@@ -48,12 +48,3 @@ if inquiry:
             response = zhipuai(st.session_state[llm])
             st.session_state[llm].append(response)
     st.rerun()
-
-
-    [
-            {"role": "user", "content": "作为一名营销专家，请为我的产品创作一个吸引人的slogan"},
-            {"role": "assistant", "content": "当然，为了创作一个吸引人的slogan，请告诉我一些关于您产品的信息"},
-            {"role": "user", "content": "智谱AI开放平台"},
-            {"role": "assistant", "content": "智启未来，谱绘无限一智谱AI，让创新触手可及!"},
-            {"role": "user", "content": "创造一个更精准、吸引人的slogan"}
-        ]
