ec73b6b0f93ed08b6188523dd905ac08329cd412
dearfad <dearfad@gmail.com>
2024-02-07 11:09:30 +0800

    update

--- multivbp.py
+++ multivbp.py
@@ -56,7 +56,7 @@ def zhipuai_chat(messages):
 
 def qwen_chat(messages):
     response = Generation.call(
-        model='qwen-1.8b-chat',
+        model='qwen-plus',
         messages=messages,
         seed=random.randint(1, 10000),
         result_format='message'
